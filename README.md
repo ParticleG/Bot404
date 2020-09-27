7a0709c589069fdb504bab83d1dad2b03768326d
Cyame <1362941473@qq.com>
2020-09-27 08:06:45 +0800

    feat: 🎸 Add demo scripts using youtube-dlc

---
+++ README.md
@@ -0,0 +1,9 @@
+# BOT404
+
+A auto video downloader plugin for bot based on CQ and youtube-dlc.
+
+## Requiremets
+
+```shell
+pip install -r requirements.txt
+```
---
+++ examples/download-demo.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+import youtube_dlc
+import logging
+
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+def tracker(d):
+    if d['status'] == 'finished':
+        print('Completely Downloaded. Converting...')
+
+
+options = {
+    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+    'progress_hooks': [tracker],
+    'merge_output_format': 'mp4',
+    'logger': logger,
+    'proxy': 'http://127.0.0.1:1080'
+}
+
+with youtube_dlc.YoutubeDL(options) as ytdlc:
+    # 夸宝
+    ytdlc.download(['https://www.youtube.com/watch?v=coIo0JtK-yA'])

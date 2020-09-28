c26a0c6f1b345d07a57aa42ba157fff62b4c063d
Marlon <1362941473@qq.com>
2020-09-28 10:52:30 +0800

    feat: 🎸 Add postprocess to download demo with log modification

--- examples/download-demo.py
+++ examples/download-demo.py
@@ -2,14 +2,26 @@ from __future__ import unicode_literals
 import youtube_dlc
 import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
+# logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logger = logging.getLogger(__name__)
+
+class logger(object):
+    def debug(self, msg):
+        print(msg)
+        pass
+    def warning(self, msg):
+        print(msg)
+        pass
+    def error(self, msg):
+        print(msg)
+        pass
 
 
 def tracker(d):
-    if d['status'] == 'downloading':
-        percent = d['_percent_str']
-        print(d)
+    # print(d)
+    # if d['status'] == 'downloading':
+    #     percent = d['_percent_str']
+    #     print(d)
     if d['status'] == 'finished':
         print('Completely Downloaded. Converting...')
 
@@ -18,11 +30,18 @@ options = {
     'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
     'progress_hooks': [tracker],
     'merge_output_format': 'mp4',
-    'logger': logger,
-    'proxy': 'http://127.0.0.1:10809'
+    'postprocessors':[
+        {
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4'
+        }
+    ],
+    # 'logger': logger,
+    'logger': logger(),
+    # 'proxy': 'http://127.0.0.1:10809'
 }
 
 if __name__ == '__main__':
     with youtube_dlc.YoutubeDL(options) as yt_dlc:
         # 夸宝好き
-        yt_dlc.download(['https://www.youtube.com/watch?v=coIo0JtK-yA'])
+        yt_dlc.download(['https://www.bilibili.com/video/BV1ZK4y1a7Tm'])

9fb78f2efab59489b4714f4a41ef19658bcd5ffe
Particle_G <gzy1135989508@gmail.com>
2020-09-29 22:06:46 +0800

    update code

--- Bot404/plugins/youtube_downloader.py
+++ Bot404/plugins/youtube_downloader.py
@@ -1,14 +1,17 @@
 from __future__ import unicode_literals
 from nonebot import on_command, CommandSession
 import nonebot
+import aiocqhttp
 import youtube_dlc
 import threading
 import datetime
 import shutil
 import re
 
-download_list = {'message_type:user_id:message_id': ['state_str', 'percent_str', 'YYYY-MM-DD_hh-mm-ss', 'title', 'uploader_id']}
+download_list = {}
 
+
+# 'message_type:user_id:message_id': ['state_str', 'percent_str', 'YYYY-MM-DD_hh-mm-ss', 'title', 'uploader_id']
 
 def _cq_at_parser(user_id):
     return "[CQ:at,qq=" + str(user_id) + "]"
@@ -16,13 +19,19 @@ def _cq_at_parser(user_id):
 
 def send_message_auto(session, message):
     bot = nonebot.get_bot()
-    print(session.event)
     if session.event.message_type == 'private':
         bot.sync.send_private_msg(user_id=session.event.user_id, message=message)
     elif session.event.message_type == 'group':
-        bot.sync.send_group_msg(group_id=session.event.group_id, message=message)
+        try:
+            bot.sync.send_group_msg(group_id=session.event.group_id, message=message)
+        except aiocqhttp.exceptions.ActionFailed:
+            bot.sync.send_private_msg(user_id=session.event.user_id, message='Exception on GroupMessage: ' + message)
+
     else:
-        bot.sync.send_discuss_msg(discuss_id=session.event.discuss_id, message=message)
+        try:
+            bot.sync.send_discuss_msg(discuss_id=session.event.discuss_id, message=message)
+        except aiocqhttp.exceptions.ActionFailed:
+            bot.sync.send_private_msg(user_id=session.event.user_id, message='Exception on DiscussMessage: ' + message)
 
 
 def upload_video(session, video_info):
@@ -45,6 +54,7 @@ def upload_video(session, video_info):
     else:
         shutil.copy(f'/usr/go-cqhttp/quin33/caches/{video_info[2]}.mp4', f'/usr/drive/holoen/others/{new_filename}.mp4')
         send_message_auto(session, '成功上传视频：“' + video_info[3] + '”到Others盘')
+    download_list.pop(session.event.message_type + ':' + str(session.event.user_id) + ':' + str(session.event.message_id))
 
 
 def download_video(session, url):
@@ -58,7 +68,6 @@ def download_video(session, url):
                         video_info[0] = 'uploading'
                     send_message_auto(session, '成功转码视频：“' + video_info[3] + '”，开始上传到OneDrive')
                     upload_video(session, video_info)
-                    download_list.pop(session.event.message_type + ':' + str(session.event.user_id) + ':' + str(session.event.message_id))
 
         @staticmethod
         def warning(msg):
@@ -99,14 +108,14 @@ def download_video(session, url):
         ],
         'outtmpl': f'../caches/{now_time}.%(ext)s',
     }
-
-    with youtube_dlc.YoutubeDL(options) as yt_dlc:
+    with youtube_dlc.YoutubeDL() as yt_dlc:
         info_dict = yt_dlc.extract_info(url, download=False)
         download_list[session.event.message_type + ':' + str(session.event.user_id) + ':' + str(session.event.message_id)] = ['Waiting',
                                                                                                                               '  0.0%',
                                                                                                                               now_time,
                                                                                                                               info_dict['title'],
                                                                                                                               info_dict['uploader_id']]
+    with youtube_dlc.YoutubeDL(options) as yt_dlc:
         yt_dlc.download([url])
 
 
@@ -117,7 +126,7 @@ async def get():
 
 @on_command('queue', aliases=('qu', '任务列表', '队列'), only_to_me=False, shell_like=True)
 async def queue(session: CommandSession):
-    session.get('res')
+    await session.send(session.get('res'))
 
 
 @get.args_parser
@@ -131,6 +140,7 @@ async def _(session: CommandSession):
         with youtube_dlc.YoutubeDL() as yt_dlc:
             info_dict = yt_dlc.extract_info(args[0], download=False)
             session.finish(message="视频：“" + info_dict['title'] + "”加入下载队列")
+        return
 
     session.pause('扒源 指令仅支持 1 个参数')
 
@@ -142,6 +152,8 @@ async def _(session: CommandSession):
     if len(args) == 0:
         response = '下载队列：\n'
         for video_info in download_list:
-            response += '| 视频名：' + video_info[3] + ' | ' + video_info[0] + ' | ' + video_info[1] + ' | 开始时间：' + video_info[2] + ' |\n'
+            print(video_info)
+            response += '| 视频名：' + str(video_info[3]) + ' | ' + str(video_info[0]) + ' | ' + str(video_info[1]) + ' | 开始时间：' + str(video_info[2]) + ' |\n'
         session.state['res'] = response
+        return
     session.pause('任务列表 指令仅支持 0 个参数')

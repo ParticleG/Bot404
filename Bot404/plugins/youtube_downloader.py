521b7c383f0580ecd674b9474580dbfcf8b898ab
Cyame <1362941473@qq.com>
2020-10-09 09:34:53 +0800

    feat: 🎸 Update@1009

--- Bot404/plugins/youtube_downloader.py
+++ Bot404/plugins/youtube_downloader.py
@@ -1,162 +1,108 @@
 from __future__ import unicode_literals
 from nonebot import on_command, CommandSession
+from Bot404.utils.configs import *
+from Bot404.utils.cq_utils import *
 import nonebot
-import aiocqhttp
 import youtube_dlc
 import threading
 import datetime
-import shutil
-import re
-import traceback
 import logging
+import shutil
+import os
+import re
 
-download_list = {}
-
-
-# 'message_type:user_id:message_id': ['state_str', 'percent_str', 'YYYY-MM-DD_hh-mm-ss', 'title', 'uploader_id']
-
-def _cq_at_parser(user_id):
-    return "[CQ:at,qq=" + str(user_id) + "]"
-
-
-def send_message_auto(session, message):
-    bot = nonebot.get_bot()
-    if session.event.message_type == 'private':
-        bot.sync.send_private_msg(user_id=session.event.user_id, message=message)
-    elif session.event.message_type == 'group':
-        try:
-            bot.sync.send_group_msg(group_id=session.event.group_id, message=message)
-        except aiocqhttp.exceptions.ActionFailed:
-            logging.error(f"未能成功发送群消息：{traceback.format_exc()}")
-            bot.sync.send_private_msg(user_id=session.event.user_id,
-                                      message='[CQ ActionFailed]Exception on GroupMessage: ' +
-                                      message)
-        except Exception:
-            logging.error(f"未知错误-未能成功发送群消息：{traceback.format_exc()}")
-            bot.sync.send_private_msg(user_id=session.event.user_id,
-                                      message='[Unknown ERROR]Unable to send GroupMessage: ' +
-                                      message)
-
-    else:
-        try:
-            bot.sync.send_discuss_msg(discuss_id=session.event.discuss_id, message=message)
-        except aiocqhttp.exceptions.ActionFailed:
-            logging.error(f"未能成功发送讨论组消息：{traceback.format_exc()}")
-            bot.sync.send_private_msg(user_id=session.event.user_id,
-                                      message='[CQ ActionFailed]Exception on DiscussMessage: ' +
-                                      message)
-        except Exception:
-            logging.error(f"未知错误-未能成功发送讨论组消息：{traceback.format_exc()}")
-            bot.sync.send_private_msg(user_id=session.event.user_id,
-                                      message='[Unknown ERROR]Unable to send DiscussMessage: ' +
-                                      message)
+download_list = {}  # {'timestamp': 'info_dict'}
 
 
 def upload_video(session, video_info):
-    new_filename = re.sub(r'[\\/:*?"<>|]', '_', video_info[3])
+    new_filename = re.sub(r'[\\/:*?"<>|]', '_', video_info['title'])
+    group_name = 'others'
     try:
-        if video_info[4] == 'UCoSrY_IQQVpmIRZ9Xf-y93g':
-            shutil.copy(f'/usr/go-cqhttp/quin33/caches/{video_info[2]}.mp4', f'/usr/drive/holoen/gura/{new_filename}.mp4')
-            send_message_auto(session, '成功上传视频：“' + video_info[3] + '”到Gura组盘')
-        elif video_info[4] == 'UCyl1z3jo3XHR1riLFKG5UAg':
-            shutil.copy(f'/usr/go-cqhttp/quin33/caches/{video_info[2]}.mp4', f'/usr/drive/holoen/amelia/{new_filename}.mp4')
-            send_message_auto(session, '成功上传视频：“' + video_info[3] + '”到Amelia组盘')
-        elif video_info[4] == 'UCMwGHR0BTZuLsmjY_NT5Pwg':
-            shutil.copy(f'/usr/go-cqhttp/quin33/caches/{video_info[2]}.mp4', f'/usr/drive/holoen/ina/{new_filename}.mp4')
-            send_message_auto(session, '成功上传视频：“' + video_info[3] + '”到Ina组盘')
-        elif video_info[4] == 'UCHsx4Hqa-1ORjQTh9TYDhww':
-            shutil.copy(f'/usr/go-cqhttp/quin33/caches/{video_info[2]}.mp4', f'/usr/drive/holoen/kiara/{new_filename}.mp4')
-            send_message_auto(session, '成功上传视频：“' + video_info[3] + '”到Kiara组盘')
-        elif video_info[4] == 'UCL_qhgtOy0dy1Agp8vkySQg':
-            shutil.copy(f'/usr/go-cqhttp/quin33/caches/{video_info[2]}.mp4', f'/usr/drive/holoen/callio/{new_filename}.mp4')
-            send_message_auto(session, '成功上传视频：“' + video_info[3] + '”到Callio组盘')
+        if video_info['uploader_id'] == 'UCoSrY_IQQVpmIRZ9Xf-y93g':
+            group_name = 'gura'
+        elif video_info['uploader_id'] == 'UCyl1z3jo3XHR1riLFKG5UAg':
+            group_name = 'amelia'
+        elif video_info['uploader_id'] == 'UCMwGHR0BTZuLsmjY_NT5Pwg':
+            group_name = 'ina'
+        elif video_info['uploader_id'] == 'UCHsx4Hqa-1ORjQTh9TYDhww':
+            group_name = 'kiara'
+        elif video_info['uploader_id'] == 'UCL_qhgtOy0dy1Agp8vkySQg':
+            group_name = 'callio'
         else:
-            shutil.copy(f'/usr/go-cqhttp/quin33/caches/{video_info[2]}.mp4', f'/usr/drive/holoen/others/{new_filename}.mp4')
-            send_message_auto(session, '成功上传视频：“' + video_info[3] + '”到Others盘')
-        download_list.pop(session.event.message_type + ':' + str(session.event.user_id) + ':' + str(session.event.message_id))
+            pass
+        shutil.copy(f'{CACHE_PATH}{video_info["now_time"]}.mp4', f'{DRIVE_PATH}{group_name}/{new_filename}.mp4')
+        send_message_auto(session, '成功下载视频：“' + video_info['title'] + f'”，已上传到{group_name}盘')
+        os.remove(f'{CACHE_PATH}{video_info["now_time"]}.mp4')
     except FileNotFoundError:
-        logging.error(f"上传时未能找到文件：{traceback.format_exc()}")
-        bot.sync.send_group_msg(group_id=session.event.group_id,
-                                message="上传失败，请手动上传临时文件。")
+        exception_handler(f'未能上传文件：找不到源文件\n{video_info["now_time"]}.mp4 -> {new_filename}.mp4', logging.ERROR)
     except Exception:
-        logging.error(f"上传时出现未知错误：{traceback.format_exc()}")
-        bot.sync.send_group_msg(group_id=session.event.group_id,
-                                message="上传失败：出现未知错误。")
+        exception_handler(f'未能上传文件：未知错误，请查看日志\n{video_info["now_time"]}.mp4 -> {new_filename}.mp4', logging.ERROR)
 
-def download_video(session, url):
+
+def is_duplicated(info_dict):
+    for video_info in download_list.values():
+        if info_dict['id'] == video_info['id']:
+            return True
+    return False
+
+
+def download_video(session, time):
+    bot = nonebot.get_bot()
+    video_info = download_list[time]
+    logging.info("Start Downloading...")
+
     class Logger(object):
         @staticmethod
         def debug(msg):
             if 'Deleting' in msg:
                 if '.flv' in msg or '.m4a' in msg:
-                    video_info = download_list[session.event.message_type + ':' + str(session.event.user_id) + ':' + str(session.event.message_id)]
-                    if video_info[0] != 'uploading':
-                        video_info[0] = 'uploading'
-                    send_message_auto(session, '成功转码视频：“' + video_info[3] + '”，开始上传到OneDrive')
+                    if video_info['status'] != '上传中':
+                        video_info['status'] = '上传中'
+                    # send_message_auto(session, '成功转码视频：“' + video_info['title'] + '”，开始上传到OneDrive')
                     upload_video(session, video_info)
+                    if download_list:
+                        download_list.pop(time)
+                    else:
+                        print("[WARN]Download List not exist.")
 
         @staticmethod
         def warning(msg):
-            print('Warning: ' + msg)
-            nonebot.get_bot().sync.send_private_msg(user_id=1135989508, message="Warning: " + msg)
+            exception_handler(f'YoutubeDLC：{msg}', logging.WARNING)
 
         @staticmethod
         def error(msg):
-            print('Error: ' + msg)
-            nonebot.get_bot().sync.send_private_msg(user_id=1135989508, message="Error: " + msg)
+            exception_handler(f'YoutubeDLC：{msg}', logging.ERROR)
 
     def tracker(data):
+        nonlocal video_info
         if data['status'] == 'downloading':
-            video_info = download_list[session.event.message_type + ':' + str(session.event.user_id) + ':' + str(session.event.message_id)]
-            if video_info[0] != 'downloading':
-                video_info[0] = 'downloading'
-            video_info[1] = data['_percent_str']
-        if data['status'] == 'finished':
-            if '.flv' in data['filename'] or '.m4a' in data['filename']:
-                video_info = download_list[session.event.message_type + ':' + str(session.event.user_id) + ':' + str(session.event.message_id)]
-                if video_info[0] != 'converting':
-                    video_info[0] = 'converting'
-                send_message_auto(session, '成功下载视频：“' + video_info[3] + '”，开始转码')
-
-    print("Start Downloading...")
-    now_time = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+            video_info['status'] = '已下载' + data['_percent_str']
+        if data['status'] == 'finished' and ('.flv' in data['filename'] or '.m4a' in data['filename']):
+            if video_info['status'] != '转码中':
+                video_info['status'] = '转码中'
+            # send_message_auto(session, '成功下载视频：“' + video_info['title'] + '”，开始转码')
 
     options = {
         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'progress_hooks': [tracker],
         'logger': Logger(),
         'merge_output_format': 'mp4',
-        'postprocessors': [
-            {
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4'
-            }
-        ],
-        'outtmpl': f'/usr/go-cqhttp/quin33/caches/{now_time}.%(ext)s',
+        'postprocessors': [{'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4'}],
+        'outtmpl': f'{CACHE_PATH}{time}.%(ext)s',
     }
     try:
-        with youtube_dlc.YoutubeDL() as yt_dlc:
-            info_dict = yt_dlc.extract_info(url, download=False)
-            download_list[session.event.message_type + ':' +
-                        str(session.event.user_id) + ':' +
-                        str(session.event.message_id)] = [
-                            'Waiting', '  0.0%', now_time, info_dict['title'],
-                            info_dict['uploader_id']
-                        ]
         with youtube_dlc.YoutubeDL(options) as yt_dlc:
-            yt_dlc.download([url])
+            yt_dlc.download([video_info['webpage_url']])
     except Exception:
-        logging.error(f"下载时出现未知错误：{traceback.format_exc()}")
-        bot.sync.send_group_msg(group_id=session.event.group_id,
-                                message="下载时出现错误，请排除队列错误后再试。")
+        exception_handler(f'下载失败：下载过程中出现错误，请排除队列错误后再试。', logging.ERROR, False, session.event.group_id)
 
 
 @on_command('get', aliases=('dl', 'download', '扒源', '扒'), only_to_me=False, shell_like=True)
-async def get():
-    pass
+async def get(session: CommandSession):
+    await session.send(session.get('res'))
 
 
-@on_command('queue', aliases=('qu', '任务列表', '队列'), only_to_me=False, shell_like=True)
+@on_command('queue', aliases=('qu', '扒源队列', '队列'), only_to_me=False, shell_like=True)
 async def queue(session: CommandSession):
     await session.send(session.get('res'))
 
@@ -166,18 +112,27 @@ async def _(session: CommandSession):
     args = session.current_arg_text.strip().split()
 
     if len(args) == 1:
-        download_thread = threading.Thread(target=download_video, args=(session, args[0]))
-        download_thread.start()
-    try:
-        with youtube_dlc.YoutubeDL() as yt_dlc:
-            info_dict = yt_dlc.extract_info(args[0], download=False)
-            session.finish(message="视频：“" + info_dict['title'] + "”加入下载队列")
+        try:
+            with youtube_dlc.YoutubeDL() as yt_dlc:
+                info_dict = yt_dlc.extract_info(args[0], download=False)
+                if is_duplicated(info_dict):
+                    session.state['res'] = '视频：“' + info_dict["title"] + '”已在队列中，请勿重复下载'
+                    return
+            info_dict['status'] = '等待中'
+            info_dict['now_time'] = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
+            download_list[info_dict['now_time']] = info_dict
+            download_thread = threading.Thread(target=download_video, args=(session, info_dict['now_time']))
+            download_thread.start()
+            session.state['res'] = '视频：“' + info_dict["title"] + '”加入下载队列'
+        except youtube_dlc.utils.UnsupportedError:
+            exception_handler(f'解析失败：视频地址不受支持，请确认地址是否正确后再试。', logging.WARNING, False, session.event.group_id)
+        except youtube_dlc.utils.DownloadError as dl_error:
+            exception_handler(f'解析失败：{dl_error.exc_info}', logging.ERROR)
+        except Exception as e:
+            exception_handler(f'解析失败：未知错误，请查看运行日志。', logging.ERROR, False, session.event.group_id)
         return
-    except Exception:
-        logging.error(f"解析时出现位置错误：{traceback.format_exc()}")
-        bot.sync.send_group_msg(group_id=session.event.group_id,
-                                message="解析时出现错误，请确认地址是否正确后再试。")
-    session.pause('扒源 指令仅支持 1 个参数')
+
+    session.pause('指令“扒源” 仅支持 1 个参数')
 
 
 @queue.args_parser
@@ -185,9 +140,9 @@ async def _(session: CommandSession):
     args = session.current_arg_text.strip().split()
 
     if len(args) == 0:
-        response = '下载队列：\n'
+        response = '扒源队列：\n'
         for video_info in download_list.values():
-            response += '| 视频名：' + str(video_info[3]) + ' | ' + str(video_info[0]) + ' | ' + str(video_info[1]) + ' | 开始时间：' + str(video_info[2]) + ' |\n'
+            response += '| 视频名：' + str(video_info['title']) + ' | ' + str(video_info['status']) + ' |\n'
         session.state['res'] = response
         return
-    session.pause('任务列表 指令仅支持 0 个参数')
+    session.pause('指令“任务列表”仅支持 0 个参数')
---
+++ Bot404/utils/cq_utils.py
@@ -0,0 +1,65 @@
+import aiocqhttp
+from nonebot import get_bot
+import traceback
+import logging
+
+
+def exception_handler(message='运行出现异常，请查看日志', log_level=logging.DEBUG, is_private=True, receiver=1135989508):
+    bot = get_bot()
+    if is_private:
+        if log_level == logging.DEBUG:
+            logging.debug(f"{message}\n{traceback.format_exc()}")
+            bot.sync.send_private_msg(user_id=receiver, message=f'[Debug] {message}')
+        elif log_level == logging.INFO:
+            logging.info(f"{message}\n{traceback.format_exc()}")
+            bot.sync.send_private_msg(user_id=receiver, message=f'[Info] {message}')
+        elif log_level == logging.WARNING:
+            logging.warning(f"{message}\n{traceback.format_exc()}")
+            bot.sync.send_private_msg(user_id=receiver, message=f'[Warning] {message}')
+        elif log_level == logging.ERROR:
+            logging.error(f"{message}\n{traceback.format_exc()}")
+            bot.sync.send_private_msg(user_id=receiver, message=f'[Error] {message}')
+        elif log_level == logging.CRITICAL:
+            logging.critical(f"{message}\n{traceback.format_exc()}")
+            bot.sync.send_private_msg(user_id=receiver, message=f'[Critical] {message}')
+    else:
+        try:
+            if log_level == logging.DEBUG:
+                logging.debug(f"{message}\n{traceback.format_exc()}")
+                bot.sync.send_group_msg(group_id=receiver, message=f'[Debug] {message}')
+            elif log_level == logging.INFO:
+                logging.info(f"{message}\n{traceback.format_exc()}")
+                bot.sync.send_group_msg(group_id=receiver, message=f'[Info] {message}')
+            elif log_level == logging.WARNING:
+                logging.warning(f"{message}\n{traceback.format_exc()}")
+                bot.sync.send_group_msg(group_id=receiver, message=f'[Warning] {message}')
+            elif log_level == logging.ERROR:
+                logging.error(f"{message}\n{traceback.format_exc()}")
+                bot.sync.send_group_msg(group_id=receiver, message=f'[Error] {message}')
+            elif log_level == logging.CRITICAL:
+                logging.critical(f"{message}\n{traceback.format_exc()}")
+                bot.sync.send_group_msg(group_id=receiver, message=f'[Critical] {message}')
+        except aiocqhttp.exceptions.ActionFailed:
+            exception_handler(f'未能成功发送群消息：账号可能被风控\n{receiver} <- {message}', logging.WARNING)
+        except Exception:
+            exception_handler(f'未能成功发送群消息：未知错误，请查看日志\n{receiver} <- {message}', logging.ERROR)
+
+
+def send_message_auto(session, message):
+    bot = get_bot()
+    if session.event.message_type == 'private':
+        bot.sync.send_private_msg(user_id=session.event.user_id, message=message)
+    elif session.event.message_type == 'group':
+        try:
+            bot.sync.send_group_msg(group_id=session.event.group_id, message=message)
+        except aiocqhttp.exceptions.ActionFailed:
+            exception_handler(f'未能成功发送群消息：账号可能被风控\n{session.event.group_id} <- {message}', logging.WARNING)
+        except Exception:
+            exception_handler(f'未能成功发送群消息：未知错误，请查看日志\n{session.event.group_id} <- {message}', logging.ERROR)
+    else:
+        try:
+            bot.sync.send_discuss_msg(discuss_id=session.event.discuss_id, message=message)
+        except aiocqhttp.exceptions.ActionFailed:
+            exception_handler(f'未能成功发送讨论组消息：账号可能被风控\n{session.event.discuss_id} <- {message}', logging.WARNING)
+        except Exception:
+            exception_handler(f'未能成功发送讨论组消息：未知错误，请查看日志\n{session.event.discuss_id} <- {message}', logging.ERROR)

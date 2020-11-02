ecba7eac61aa067e129d2114a54e6851511a3773
Cyame <1362941473@qq.com>
2020-11-02 21:49:22 +0800

    feat: 🎸 add spliter

--- Bot404/plugins/youtube_downloader.py
+++ Bot404/plugins/youtube_downloader.py
@@ -1,7 +1,10 @@
 from __future__ import unicode_literals
 from nonebot import on_command, CommandSession
+
 from Bot404.utils.configs import *
 from Bot404.utils.cq_utils import *
+from Bot404.utils.ytdl_spliter import *
+
 import youtube_dlc
 import threading
 import datetime
@@ -29,13 +32,26 @@ def upload_video(session, video_info):
             group_name = 'callio'
         else:
             pass
-        shutil.copy(f'{CACHE_PATH}{video_info["now_time"]}.mp4', f'{DRIVE_PATH}{group_name}/{new_filename}.mp4')
-        send_message_auto(session, '成功下载视频：“' + video_info['title'] + f'”，已上传到{group_name}盘')
+        split_count = execute_split(
+            f'{CACHE_PATH}{video_info["now_time"]}.mp4')
+        for clip in range(1, split_count):
+            shutil.copy(f'{CACHE_PATH}{video_info["now_time"]}_{clip}.mp4',
+                        f'{DRIVE_PATH}{group_name}/{new_filename}_{clip}.mp4')
+
+        # shutil.rmtree(f'{CACHE_PATH}{video_info["now_time"]}.mp4')
+        # shutil.copy()
+        # shutil.copy(f'{CACHE_PATH}{video_info["now_time"]}.mp4', f'{DRIVE_PATH}{group_name}/{new_filename}.mp4')
+        send_message_auto(
+            session, '成功下载视频：“' + video_info['title'] + f'”，已上传到{group_name}盘')
         os.remove(f'{CACHE_PATH}{video_info["now_time"]}.mp4')
     except FileNotFoundError:
-        exception_handler(f'未能上传文件：找不到源文件\n{video_info["now_time"]}.mp4 -> {new_filename}.mp4', logging.ERROR)
+        exception_handler(
+            f'未能上传文件：找不到源文件\n{video_info["now_time"]}.mp4 -> {new_filename}.mp4',
+            logging.ERROR)
     except Exception:
-        exception_handler(f'未能上传文件：未知错误，请查看日志\n{video_info["now_time"]}.mp4 -> {new_filename}.mp4', logging.ERROR)
+        exception_handler(
+            f'未能上传文件：未知错误，请查看日志\n{video_info["now_time"]}.mp4 -> {new_filename}.mp4',
+            logging.ERROR)
 
 
 def is_duplicated(info_dict):
@@ -75,27 +91,39 @@ def download_video(session, time):
         nonlocal video_info
         if data['status'] == 'downloading':
             video_info['status'] = '已下载' + data['_percent_str']
-        if data['status'] == 'finished' and ('.flv' in data['filename'] or '.m4a' in data['filename']):
+        if data['status'] == 'finished' and ('.flv' in data['filename']
+                                             or '.m4a' in data['filename']):
             if video_info['status'] != '转码中':
                 video_info['status'] = '转码中'
             # send_message_auto(session, '成功下载视频：“' + video_info['title'] + '”，开始转码')
 
     options = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'format':
+        'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'progress_hooks': [tracker],
-        'logger': Logger(),
-        'merge_output_format': 'mp4',
-        'postprocessors': [{'key': 'FFmpegVideoConvertor', 'preferedformat': 'mp4'}],
-        'outtmpl': f'{CACHE_PATH}{time}.%(ext)s',
+        'logger':
+        Logger(),
+        'merge_output_format':
+        'mp4',
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4'
+        }],
+        'outtmpl':
+        f'{CACHE_PATH}{time}.%(ext)s',
     }
     try:
         with youtube_dlc.YoutubeDL(options) as yt_dlc:
             yt_dlc.download([video_info['webpage_url']])
     except Exception:
-        exception_handler(f'下载失败：下载过程中出现错误，请排除队列错误后再试。', logging.ERROR, False, session.event.group_id)
+        exception_handler(f'下载失败：下载过程中出现错误，请排除队列错误后再试。', logging.ERROR, False,
+                          session.event.group_id)
 
 
-@on_command('get', aliases=('dl', 'download', '扒源', '扒'), only_to_me=False, shell_like=True)
+@on_command('get',
+            aliases=('dl', 'download', '扒源', '扒'),
+            only_to_me=False,
+            shell_like=True)
 async def get(session: CommandSession):
     args = session.current_arg_text.strip().split()
 
@@ -104,35 +132,42 @@ async def get(session: CommandSession):
             with youtube_dlc.YoutubeDL() as yt_dlc:
                 info_dict = yt_dlc.extract_info(args[0], download=False)
                 if is_duplicated(info_dict):
-                    await session.send('视频：“' + info_dict["title"] + '”已在队列中，请勿重复下载')
+                    await session.send('视频：“' + info_dict["title"] +
+                                       '”已在队列中，请勿重复下载')
                     return
             info_dict['status'] = '等待中'
-            info_dict['now_time'] = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S.%f')
+            info_dict['now_time'] = datetime.datetime.now().strftime(
+                '%Y-%m-%d_%H:%M:%S.%f')
             download_list[info_dict['now_time']] = info_dict
-            download_thread = threading.Thread(target=download_video, args=(session, info_dict['now_time']))
+            download_thread = threading.Thread(target=download_video,
+                                               args=(session,
+                                                     info_dict['now_time']))
             download_thread.start()
             await session.send('视频：“' + info_dict["title"] + '”加入下载队列')
         except youtube_dlc.utils.UnsupportedError:
-            exception_handler(f'解析失败：视频地址不受支持，请确认地址是否正确后再试。', logging.WARNING, False, session.event.group_id)
+            exception_handler(f'解析失败：视频地址不受支持，请确认地址是否正确后再试。', logging.WARNING,
+                              False, session.event.group_id)
         except youtube_dlc.utils.DownloadError as dl_error:
             exception_handler(f'解析失败：{dl_error.exc_info}', logging.ERROR)
         except Exception as e:
-            exception_handler(f'解析失败：未知错误，请查看运行日志。', logging.ERROR, False, session.event.group_id)
+            exception_handler(f'解析失败：未知错误，请查看运行日志。', logging.ERROR, False,
+                              session.event.group_id)
         return
     else:
         await session.send("参数个数错误，仅支持一个参数，请检查后再试。")
 
 
-@on_command('queue', aliases=('qu', '扒源队列', '队列'), only_to_me=False, shell_like=True)
+@on_command('queue',
+            aliases=('qu', '扒源队列', '队列'),
+            only_to_me=False,
+            shell_like=True)
 async def queue(session: CommandSession):
     # args = session.current_arg_text.strip().split()
     if len(download_list.values()) != 0:
         response = '扒源队列：\n'
         for video_info in download_list.values():
-            response += '| 视频名：' + str(video_info['title']) + ' | ' + str(video_info['status']) + ' |\n'
+            response += '| 视频名：' + str(video_info['title']) + ' | ' + str(
+                video_info['status']) + ' |\n'
     else:
         response = '扒源队列为空'
     await session.send(response)
-
-
-
--- Bot404/utils/ytdl_spliter.py
+++ Bot404/utils/ytdl_spliter.py
@@ -1,16 +1,18 @@
+# Have to run under python3.8 with latest requirements.
 import asyncio
 from ffmpeg import FFmpeg  # python-ffmpeg==1.0.11
 from ffprobe import FFProbe  # ffprobe-python==1.0.3
 
-import asyncio
-from ffmpeg import FFmpeg  # python-ffmpeg==1.0.11
-from ffprobe import FFProbe  # ffprobe-python==1.0.3
+MINIMUM_ONE_FRAME = 0.01667
 
+def if_end(current_duration, duration_seconds):
+    return abs(current_duration - duration_seconds) >= MINIMUM_ONE_FRAME
 
 async def run(_video_filename, _size, _index, _current_duration):
+    video_name = _video_filename[:-4]
     ffmpegProcess = FFmpeg().input(_video_filename, {
         'ss': _current_duration
-    }).option('-y').output(f'test_{_index}.mp4', {
+    }).option('-y').output(f'{video_name}_{_index}.mp4', {
         'fs': _size,
         'c': 'copy'
     })
@@ -36,25 +38,29 @@ def split_video(video_filename, size, index, current_duration):
             _current_duration=current_duration))
     loop.close()
 
-
-if __name__ == '__main__':
-    metadata = FFProbe('test.mp4')
+def execute_split(video):
+    # LOAD
+    video_metadata = FFProbe(video)
+    video_name = video[:-4]
+    # INIT
     duration_seconds = 0
     current_duration = 0
     index = 1
-
-    for stream in metadata.streams:
+    # GET VIDEO STREAM
+    for stream in video_metadata.streams:
         if stream.is_video():
-            print(f'Video last for {stream.duration_seconds()} seconds.')
+            print(f'[Origin Video] "{video}" last for {stream.duration_seconds()} seconds.')
             duration_seconds = stream.duration_seconds()
-    # 60fps: 1fps = 0.01667 小于一帧就算了.jpg
+    # SPLIT
     while abs(current_duration - duration_seconds) >= 0.01667:
-        split_video('test.mp4', '20M', index, current_duration)
-        nextMetadata = FFProbe(f'test_{index}.mp4')
+        split_video(video, '1024M', index, current_duration)
+        nextMetadata = FFProbe(f'{video_name}_{index}.mp4')
         for nextStream in nextMetadata.streams:
             if nextStream.is_video():
                 print(
-                    f'CLip {index} last for {nextStream.duration_seconds()} seconds.'
+                    f'Clip{index} last for {nextStream.duration_seconds()} seconds.'
                 )
                 current_duration += nextStream.duration_seconds()
         index += 1
+    # COUNT
+    return index
